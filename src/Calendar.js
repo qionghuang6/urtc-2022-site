fd8266c8af2654fc8a6b8cb4f33d4d27e30e303c
Qiong Zhou Huang <qiongzhouh@gmail.com>
2022-09-26 10:00:55 -0400

    sort events

--- src/Calendar.js
+++ src/Calendar.js
@@ -6,9 +6,10 @@ const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sa
 
 export default function Calendar({events}) {
     const mobile = useMediaQuery('(max-width:599px)');
+    const sortedEvents = [...(events?.items)].sort((a,b) => new Date(a.start.dateTime).getTime() - new Date(b.start.dateTime).getTime());
     return (
         <>
-            {events?.items.filter(event => new Date(event.end.dateTime).getTime() > Date.now()).map(event => (
+            {sortedEvents?.filter(event => new Date(event.end.dateTime).getTime() > Date.now()).map(event => (
                 <GlassBox key={event.id} sx={{ display: 'inline-block', margin: '8px', paddingX: '20px', paddingY: '12px', width: mobile ? '95%' : 'fit-content'}} background='linear-gradient(135deg, rgba(33, 147, 200, 0.6), rgb(109, 213, 237, 0.6))'>
                     <Typography align="left" variant="h6" fontFamily="Montserrat" fontWeight="700" color="white">{event.summary}</Typography>
                     <Typography align="left" variant="body1" color="white">{weekday[new Date(event.start.dateTime).getDay()]}, {new Date(event.start.dateTime).toLocaleDateString()}</Typography>
