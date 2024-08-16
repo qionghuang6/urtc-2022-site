c4e4a21572b580baa9600e02aea0607819dd708f
Hailey <haileyrpan@gmail.com>
2024-08-16 08:39:09 -0400

    updated team

--- src/organizers/organizer_info.js
+++ src/organizers/organizer_info.js
@@ -43,17 +43,62 @@ export const organizers = [
   //   "spare_time": "",
   //   "random": ""
   // },
+
   {
-    "email": "huangq@mit.edu",
-    "name": "Qiong Zhou Huang",
-    "img": "qiong.jpg",
-    "team": "Technology Chair",
-    "intro": "I'm Qiong Zhou Huang, majoring in Electrical Engineering and Computer Science. I'm from Brooklyn, New York. 🗽",
-    "position": "As tech chair 🪑, I work on the magical technology that makes this website and other parts of the conference run.",
-    "interests": "",
-    "spare_time": "",
-    "random": ""
+    "email": "blin15@mit.edu",
+    "img": "bobo.jpeg",
+    "name": "Bobo Lin",
+    "team": "Publicity & Marketing",
+    "intro": "I'm Bobo, currently a freshman, and I intend to major in Course 18C (Mathematics with Computer Science). I'm from Chicago, Illinois.",
+    "position": "I'm one of the publicity and marketing co-chairs, and am responsible for designing the logo and spreading information about the conference.",
+    "interests": "I'm interested in the applications of math and computer science, especially in biology and medicine. I'm also interested in AI and its use in fields of computational biology and bioinformatics.",
+    "spare_time": "In my spare time, I like taking walks around my neighborhood, get coffee and matcha, and reading new books.",
+    "random": "1. I was born here in the states but raised in China. 2. My favorite drink from Dunkin is the cookie butter cold brew (highly recommend but sadly it's seasonal)."
   },
+  {
+    "email": "adarsh@mit.edu",
+    "img": "adarsh.jpeg",
+    "name": "Adarsh Iyer",
+    "team": "Logistics",
+    "intro": "I'm a first-year from San Jose, California, intending to major in Course 6-9 (Computation and Cognition).",
+    "position": "I handle logistics related to finding and coordinating with speakers and panelists as well as organizing special events at the conference.",
+    "interests": "I am fascinated by brain-inspired (neural) computing, dynamical systems, computer graphics, computer vision, and symbolic deep learning. I've done some research work in these areas.",
+    "spare_time": "As a member of the MIT Cross Country and Track teams, you'll usually find me running in my spare time. I also enjoy music composition, piano playing, 3D animation, carpentry, cooking, baking, reading, and making new friends.",
+    "random": "I once ran six miles in flip-flops and I like gnocchi."
+  },
+  {
+    "email": "mamidala@mit.edu",
+    "img": "srilekha.jpg",
+    "name": "Srilekha Mamidala",
+    "team": "Publicity & Marketing",
+    "intro": "I'm Srilekha Mamidala, and I'm a freshman interested in majoring in Course 6-14 (CS and Economics). I'm originally from Garnet Valley, Pennsylvania.",
+    "position": "I'm Publicity and Marketing Chair, which means I'm responsible for spreading awareness about the conference to make sure it's bigger than ever! From creating digital materials for sponsorships and communicating to interested presenters, it's my job to ensure URTC invites an exciting diversity of research.",
+    "interests": "I'm interested in using computer science approaches to solve issues in the environmental and human health sectors. Recently, I've also done research applying principles from LLMs (like ChatGPT!) in the legal analytics sector. I'm also focused on taking research beyond the lab and into the hands of communities with entrepreneurship.",
+    "spare_time": "I am a classical dancer and have been dancing for 11 years! I also enjoy landscape painting, love visiting new places.",
+    "random": "I've never liked potato chips, and my dream city to visit is Tokyo."
+  },
+  {
+    "email": "hanngao@mit.edu",
+    "img": "hannah.jpg",
+    "name": "Hannah Gao",
+    "team": "Logistics",
+    "intro": "I'm Hannah Gao, a sophomore majoring in 6-3. I am from Philadelphia, PA.",
+    "position": "As one of the logistics chairs, I plan and recruit for conference events such as guest speakers and panel sessions, and I make sure these events run smoothly!",
+    "interests": "All things AI; theoretical computer science",
+    "spare_time": "Some of my hobbies include playing guitar, tennis, and frisbee.",
+    "random": "1) Currently, my favorite flavor of ice cream is black sesame. 2) I love 90+ degree weather."
+  },
+  // {
+  //   "email": "huangq@mit.edu",
+  //   "name": "Qiong Zhou Huang",
+  //   "img": "qiong.jpg",
+  //   "team": "Technology Chair",
+  //   "intro": "I'm Qiong Zhou Huang, majoring in Electrical Engineering and Computer Science. I'm from Brooklyn, New York. 🗽",
+  //   "position": "As tech chair 🪑, I work on the magical technology that makes this website and other parts of the conference run.",
+  //   "interests": "",
+  //   "spare_time": "",
+  //   "random": ""
+  // },
   // {
   //   "email": "robertcc@mit.edu",
   //   "img": "robert.jpg",
@@ -65,50 +110,50 @@ export const organizers = [
   //   "spare_time": "I'm a risk-averse adventurer who enjoys hiking and bouldering, horror movie enthusiast, chess beginner, and closeted weeb.",
   //   "random": "I'm on a mission to try every Mountain Dew flavor, and my favorite movie character is Megamind."
   // },
-  {
-    "email": "",
-    "img": "karen.jpg",
-    "name": "Karen Liberman",
-    "team": "Technology",
-    "intro": "",
-    "position": "",
-    "interests": "",
-    "spare_time": "",
-    "random": ""
-  },
-  {
-    "email": "cyung@mit.edu",
-    "img": "cathy.jpg",
-    "name": "Cathy Yung",
-    "team": "Logistics Chair",
-    "intro": "I'm Cathy Yung, currently majoring in Course 18C (Mathematics with Computer Science). I am from Germantown, Maryland!",
-    "position": "I am one of the logistic chairs, so essentially, I help make sure the conference runs super smoothly. From reaching out to speakers, to organizing panel sessions, to time scheduling, I am responsible for making sure all these happen. My position also includes coordinating the venue (and in COVID years, the virtual technology) needed for the conference.",
-    "interests": "Recently, I've been pretty into technology used for assisting people who are disabled. For example, people who have been paralyzed can use technology that tracks their eye movement to communicate with others and move around independently.",
-    "spare_time": "I enjoy playing volleyball and tennis with my friends, as well as planning day trips + vacations. I'm also currently learning how to play guitar.",
-    "random": "1. I sneeze when I look at the sun. 2. I hate balsamic vinegar."
-  },
-  {
-    "email": "",
-    "img": "lucas.jpg",
-    "name": "Lucas Chu",
-    "team": "Logistics",
-    "intro": "",
-    "position": "",
-    "interests": "",
-    "spare_time": "",
-    "random": ""
-  },
-  {
-    "email": "tammyc@mit.edu",
-    "name": "Tammy Chen",
-    "img": "tammy.jpeg",
-    "team": "Advisor",
-    "intro": "I'm Tammy Chen, majoring in 6-14. I came to MIT from Brooklyn, New York.",
-    "position": "", //"I am one of this year's conference chairs which means I make sure the conference goes smoothly behind the scene. I am responsible for overseeing the different departments in URTC (logistics, technology, sponsorship, publicity) & conference planning on a large scale.",
-    "interests": "I love learning more about artificial intelligence & investigating more into the virtual reality realm.",
-    "spare_time": "I am a culprit of investing too much time into fantasy sports & analytics (particularly football and basketball) + playing Valorant. I also love taking long walks & visiting new places.",
-    "random": "Chicken nuggets with guacamole is a superior food combination + I am beyond scared of heights."
-  },
+  // {
+  //   "email": "",
+  //   "img": "karen.jpg",
+  //   "name": "Karen Liberman",
+  //   "team": "Technology",
+  //   "intro": "",
+  //   "position": "",
+  //   "interests": "",
+  //   "spare_time": "",
+  //   "random": ""
+  // },
+  // {
+  //   "email": "cyung@mit.edu",
+  //   "img": "cathy.jpg",
+  //   "name": "Cathy Yung",
+  //   "team": "Logistics Chair",
+  //   "intro": "I'm Cathy Yung, currently majoring in Course 18C (Mathematics with Computer Science). I am from Germantown, Maryland!",
+  //   "position": "I am one of the logistic chairs, so essentially, I help make sure the conference runs super smoothly. From reaching out to speakers, to organizing panel sessions, to time scheduling, I am responsible for making sure all these happen. My position also includes coordinating the venue (and in COVID years, the virtual technology) needed for the conference.",
+  //   "interests": "Recently, I've been pretty into technology used for assisting people who are disabled. For example, people who have been paralyzed can use technology that tracks their eye movement to communicate with others and move around independently.",
+  //   "spare_time": "I enjoy playing volleyball and tennis with my friends, as well as planning day trips + vacations. I'm also currently learning how to play guitar.",
+  //   "random": "1. I sneeze when I look at the sun. 2. I hate balsamic vinegar."
+  // },
+  // {
+  //   "email": "",
+  //   "img": "lucas.jpg",
+  //   "name": "Lucas Chu",
+  //   "team": "Logistics",
+  //   "intro": "",
+  //   "position": "",
+  //   "interests": "",
+  //   "spare_time": "",
+  //   "random": ""
+  // },
+  // {
+  //   "email": "tammyc@mit.edu",
+  //   "name": "Tammy Chen",
+  //   "img": "tammy.jpeg",
+  //   "team": "Advisor",
+  //   "intro": "I'm Tammy Chen, majoring in 6-14. I came to MIT from Brooklyn, New York.",
+  //   "position": "", //"I am one of this year's conference chairs which means I make sure the conference goes smoothly behind the scene. I am responsible for overseeing the different departments in URTC (logistics, technology, sponsorship, publicity) & conference planning on a large scale.",
+  //   "interests": "I love learning more about artificial intelligence & investigating more into the virtual reality realm.",
+  //   "spare_time": "I am a culprit of investing too much time into fantasy sports & analytics (particularly football and basketball) + playing Valorant. I also love taking long walks & visiting new places.",
+  //   "random": "Chicken nuggets with guacamole is a superior food combination + I am beyond scared of heights."
+  // },
   // {
   //   "email": "",
   //   "img": "ritik.jpeg",
