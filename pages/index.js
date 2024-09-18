07a5078e1f2ab6f17e8f592c8858c587788f1bb6
lablab987 <lasyabalachandran@gmail.com>
2024-09-17 21:53:01 -0400

    update conference schedule

--- pages/index.js
+++ pages/index.js
@@ -208,7 +208,7 @@ export default function Index({ events }) {
           </Grid>
           <Grid item xs={5} container direction="column" rowSpacing={4.5} align='left' >
             <Grid item>
-              <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="https://drive.google.com/file/d/1dCzb9vKWOcATE77cVMUkuG574Mgd9HlQ/view?usp=drive_link"><PictureAsPdfIcon/>{'\u00A0'}Program Overview Table (2023)</Button>
+              <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="/urtc_2024_program_table.pdf"><PictureAsPdfIcon/>{'\u00A0'}Program Overview Table (2024)</Button>
             </Grid>
             <Grid item>
               <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="https://drive.google.com/file/d/1zoajDwG-amGhkUjAp3lMRO7LIQKnMzIt/view?usp=drive_link"><PictureAsPdfIcon/>{'\u00A0'}Paper Presentation Schedule (2023) </Button>
