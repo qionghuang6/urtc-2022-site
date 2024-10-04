183cc5a7373a34c39ec619b0589b10f6cf720af4
lablab987 <lasyabalachandran@gmail.com>
2024-10-04 11:33:51 -0400

    updated schedules

--- pages/index.js
+++ pages/index.js
@@ -211,7 +211,7 @@ export default function Index({ events }) {
               <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="/urtc_2024_program_table.pdf"><PictureAsPdfIcon/>{'\u00A0'}Program Overview Table (2024)</Button>
             </Grid>
             <Grid item>
-              <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="https://drive.google.com/file/d/1zoajDwG-amGhkUjAp3lMRO7LIQKnMzIt/view?usp=drive_link"><PictureAsPdfIcon/>{'\u00A0'}Paper Presentation Schedule (2023) </Button>
+              <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="/urtc_2024_paper_presentation_schedule.pdf"><PictureAsPdfIcon/>{'\u00A0'}Paper Presentation Schedule (2024) </Button>
             </Grid>
             <Grid item>
               <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="https://drive.google.com/file/d/1RpYd-rULck2FcGRaE4YQzWQrBvJxLirQ/view?usp=drive_link"><PictureAsPdfIcon/>{'\u00A0'}Poster Presentation Schedule (2023)</Button>
