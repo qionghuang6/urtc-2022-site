54a49adf2baac31f34c2061835ba00e01217db21
lablab987 <lasyabalachandran@gmail.com>
2025-09-29 20:29:04 -0400

    2025 schedule

--- pages/index.js
+++ pages/index.js
@@ -228,7 +228,7 @@ export default function Index({ events }) {
           </Grid>
           <Grid item xs={5} container direction="column" rowSpacing={4.5} align='left' >
             <Grid item>
-              <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="/urtc_2024_program_table.pdf"><PictureAsPdfIcon/>{'\u00A0'}Program Overview Table (2024) </Button>
+              <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="/urtc_2025_program_table.pdf"><PictureAsPdfIcon/>{'\u00A0'}Program Overview Table (2025) </Button>
             </Grid>
             <Grid item>
               <Button variant="outlined" sx={{fontSize: mobile? '12px':'18px', color: 'white', borderColor: 'transparent'}} href="/urtc_2024_paper_presentation_schedule.pdf"><PictureAsPdfIcon/>{'\u00A0'}Paper Presentation Schedule (2024) </Button>
